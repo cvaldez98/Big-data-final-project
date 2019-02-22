5f95364d845b6d64c8dffedd3b59915f1666bd20
adrianaixba <ixbaadriana@gmail.com>
2019-02-22 13:00:36 +0100

    added some code and an example file for funsies

--- training/data_collection_training.py
+++ training/data_collection_training.py
@@ -1,4 +1,51 @@
+import numpy as np
+import string
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+import sklearn.svm as svm
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import confusion_matrix
+from sklearn import datasets
+from sklearn.preprocessing  import LabelEncoder
+from sklearn.metrics import recall_score
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer
+from imblearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
 # iterate through training data
+
+path = "../data/aclImdb/"
+positiveFiles = [x for x in os.listdir(path+"train/pos/") if x.endswith(".txt")]
+negativeFiles = [x for x in os.listdir(path+"train/neg/") if x.endswith(".txt")]
+testFiles = [x for x in os.listdir(path+"test/") if x.endswith(".txt")]
+positiveReviews, negativeReviews, testReviews = [], [], []
+for pfile in positiveFiles:
+    with open(path+"train/pos/"+pfile, encoding="latin1") as f:
+        positiveReviews.append(f.read())
+for nfile in negativeFiles:
+    with open(path+"train/neg/"+nfile, encoding="latin1") as f:
+        negativeReviews.append(f.read())
+for tfile in testFiles:
+    with open(path+"test/"+tfile, encoding="latin1") as f:
+        testReviews.append(f.read())
+
+reviews = pd.concat([
+    pd.DataFrame({"review":positiveReviews, "label":1, "file":positiveFiles}),
+    pd.DataFrame({"review":negativeReviews, "label":0, "file":negativeFiles}),
+    pd.DataFrame({"review":testReviews, "label":-1, "file":testFiles})
+], ignore_index=True).sample(frac=1, random_state=1)
+reviews.head()
+
+reviews = reviews[["review", "label", "file"]].sample(frac=1, random_state=1)
+train = reviews[reviews.label!=-1].sample(frac=0.6, random_state=1)
+valid = reviews[reviews.label!=-1].drop(train.index)
+test = reviews[reviews.label==-1]
 # clean it
 # vectorize the data (between countVectorizer, hashVectorizer)
-# return the vectorized matrix?
+#  return the vectorized matrix?
