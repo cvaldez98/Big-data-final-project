98f63864fd314a4e29f38dc9b760de04226d3285
Macbook <carlos.valdez@kuka.com>
2019-02-25 15:42:39 +0100

    added git ignore for .txt files, added skeleton code for data_collection_eval on evaluation

--- evaluation/data_collection_eval.py
+++ evaluation/data_collection_eval.py
@@ -1,2 +1,22 @@
-# split up data
 # cross validation here
+
+import os
+
+# give a result from the million + reviews
+
+directory = os.fsencode('../data/movie_samples/')
+
+def evaluate_snippet(filename):
+    print("evaluating",filename)
+    # evaluate review
+    # here we should use our trained classifier and run it here.
+    # maybe we should also print our statistics here?
+
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if(filename.endswith('.txt')):
+        evaluate_snippet(filename)
+        continue
+
+
