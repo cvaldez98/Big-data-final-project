1250028c1803333f00fdf9846d4dc6d5caaa1d61
adrianaixba <ixbaadriana@gmail.com>
2019-02-26 14:14:19 +0100

    fixed up code

--- training/train_svm.py
+++ training/train_svm.py
@@ -20,7 +20,7 @@ from sklearn.feature_extraction.text import HashingVectorizer
 import plotly
 import plotly.offline as py
 import plotly.graph_objs as go
-plotly.offline.init_notebook_mode(connected=True)
+# plotly.offline.init_notebook_mode(connected=True)
 
 import itertools
 import matplotlib.pyplot as plt
@@ -74,57 +74,57 @@ def vectorizer():
 
 # Compute confusion matrix
 # cnf_matrix = confusion_matrix(y_test, y_pred_count)
-tf_matrix = confusion_matrix(y_test, y_pred_tf)
-# hash_matrix = confusion_matrix(y_test, y_pred_hash)
+# tf_matrix = confusion_matrix(y_test, y_pred_tf)
+# # hash_matrix = confusion_matrix(y_test, y_pred_hash)
 
-# print(cnf_matrix)
-print(tf_matrix)
-# print(hash_matrix)
+# # print(cnf_matrix)
+# print(tf_matrix)
+# # print(hash_matrix)
 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    #This function prints and plots the confusion matrix.
-    #Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
+# def plot_confusion_matrix(cm, classes,
+#                           normalize=False,
+#                           title='Confusion matrix',
+#                           cmap=plt.cm.Blues):
+#     """
+#     #This function prints and plots the confusion matrix.
+#     #Normalization can be applied by setting `normalize=True`.
+#     """
+#     if normalize:
+#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#         print("Normalized confusion matrix")
+#     else:
+#         print('Confusion matrix, without normalization')
 
-    print(cm)
+#     print(cm)
 
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#     plt.title(title)
+#     plt.colorbar()
+#     tick_marks = np.arange(len(classes))
+#     plt.xticks(tick_marks, classes, rotation=45)
+#     plt.yticks(tick_marks, classes)
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+#     fmt = '.2f' if normalize else 'd'
+#     thresh = cm.max() / 2.
+#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+#         plt.text(j, i, format(cm[i, j], fmt),
+#                  horizontalalignment="center",
+#                  color="white" if cm[i, j] > thresh else "black")
 
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
+#     plt.ylabel('True label')
+#     plt.xlabel('Predicted label')
+#     plt.tight_layout()
 
-    np.set_printoptions(precision=2)
+#     np.set_printoptions(precision=2)
 
-# Plot normalized confusion matrix
-plt.figure()
-# plot_confusion_matrix(cnf_matrix, classes=["neg", "pos"], normalize=True,
-#                       title='Normalized count_vect confusion matrix')
-plot_confusion_matrix(tf_matrix, classes=["neg", "pos"], normalize=True,
-                      title='Normalized tf_vect confusion matrix')
-# plot_confusion_matrix(hash_matrix, classes=["neg", "pos"], normalize=True,
-#                       title='Normalized hash_vect confusion matrix')
+# # Plot normalized confusion matrix
+# plt.figure()
+# # plot_confusion_matrix(cnf_matrix, classes=["neg", "pos"], normalize=True,
+# #                       title='Normalized count_vect confusion matrix')
+# plot_confusion_matrix(tf_matrix, classes=["neg", "pos"], normalize=True,
+#                       title='Normalized tf_vect confusion matrix')
+# # plot_confusion_matrix(hash_matrix, classes=["neg", "pos"], normalize=True,
+# #                       title='Normalized hash_vect confusion matrix')
 
-plt.show()
+# plt.show()
