66472116d10381860fb63b0e3599ba072a923a4b
adrianaixba <ixbaadriana@gmail.com>
2019-02-27 14:45:32 +0100

    tested countvect, hashingvect, and tfidvect - commented out

--- training/train_svm.py
+++ training/train_svm.py
@@ -3,19 +3,21 @@ from data_collection_training import all_reviews
 
 import string
 import numpy as np
+from numpy import array
 import pandas as pd
 import sklearn.svm as svm
 from sklearn import datasets
 from sklearn.svm import LinearSVC
-from sklearn.metrics import recall_score
+
+from sklearn.metrics import recall_score, average_precision_score, precision_score
+from sklearn.metrics import classification_report, confusion_matrix
+
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing  import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
 
 import plotly
 import plotly.offline as py
@@ -29,13 +31,13 @@ labels = labels()
 review_tokens = all_reviews()
 
 # vectorizer for organizing training/testing data
-count_vect = CountVectorizer()
+# count_vect = CountVectorizer()
 tf_vect = TfidfVectorizer()
-hash_vect = HashingVectorizer()
+# hash_vect = HashingVectorizer()
 # tokens must be a list of full reviews
-count_vect.fit(review_tokens)
+# count_vect.fit(review_tokens)
 tf_vect.fit(review_tokens)
-hash_vect.fit(review_tokens)
+# hash_vect.fit(review_tokens)
 
 # split data into training and test set with train_test_split function - setting shuffle to True because, we
 # have pos reviews on the 1st half and neg on 2nd half of 'tokens' array
@@ -43,27 +45,45 @@ hash_vect.fit(review_tokens)
 x_train, x_test, y_train, y_test = train_test_split(review_tokens, labels, test_size=.5, random_state=1234, shuffle=True)
 
 # create svm classifier and train it
-count_lsvm = LinearSVC()
+# count_lsvm = LinearSVC()
 tf_lsvm = LinearSVC()
-hash_lsvm = LinearSVC()
+# hash_lsvm = LinearSVC()
 
-count_lsvm.fit(count_vect.transform(x_train), y_train)
+# count_lsvm.fit(count_vect.transform(x_train), y_train)
 tf_lsvm.fit(tf_vect.transform(x_train), y_train)
-hash_lsvm.fit(hash_vect.transform(x_train), y_train)
+# hash_lsvm.fit(hash_vect.transform(x_train), y_train)
 
 # get accuracy/performance of classifier
-count_score = count_lsvm.score(count_vect.transform(x_test), y_test)
+# count_score = count_lsvm.score(count_vect.transform(x_test), y_test)
 tf_score = tf_lsvm.score(tf_vect.transform(x_test), y_test)
-hash_score = hash_lsvm.score(hash_vect.transform(x_test), y_test)
+# hash_score = hash_lsvm.score(hash_vect.transform(x_test), y_test)
 
-print("SVM with a CountVectorizer performed with an accuracy of " + str(count_score * 100) + " %")
+
+
+# print("SVM with a CountVectorizer performed with an accuracy of " + str(count_score * 100) + " %")
 print("SVM with a TfidfVectorizer performed with an accuracy of " + str(tf_score * 100) + " %")
-print("SVM with a HashingVectorizer performed with an accuracy of " + str(hash_score * 100) + " %")
+# print("SVM with a HashingVectorizer performed with an accuracy of " + str(hash_score * 100) + " %")
 
 # y_pred_count = count_lsvm.predict(count_vect.transform(x_test))
 y_pred_tf = tf_lsvm.predict(tf_vect.transform(x_test))
 # y_pred_hash = hash_lsvm.predict(hash_vect.transform(x_test))
 # print(y_pred)
+bin_labels = ['pos', 'neg']
+recall_score = recall_score(y_test, y_pred_tf, labels=bin_labels, average=None)
+precision_score = precision_score(y_test, y_pred_tf, labels=bin_labels, average=None)
+print("The recall score with tf is " + str(recall_score))
+print("The percision score with tf is " + str(precision_score))
+
+# recall_score_2 = recall_score(y_test, y_pred_count, labels=bin_labels, average=None)
+# precision_score_2 = precision_score(y_test, y_pred_count, labels=bin_labels, average=None)
+# print("The recall score with count is " + str(recall_score_2))
+# print("The percision score with count is " + str(precision_score_2))
+
+# recall_score_3 = recall_Sscore(y_test, y_pred_hash, labels=bin_labels, average=None)
+# precision_score_3 = precision_score(y_test, y_pred_hash, labels=bin_labels, average=None)
+# print("The recall score with hashing is " + str(recall_score_3))
+# print("The percision score with hashing is " + str(precision_score_3))
+
 
 def classifier():
     return tf_lsvm
@@ -75,9 +95,9 @@ def vectorizer():
 # Compute confusion matrix
 # cnf_matrix = confusion_matrix(y_test, y_pred_count)
 # tf_matrix = confusion_matrix(y_test, y_pred_tf)
-# # hash_matrix = confusion_matrix(y_test, y_pred_hash)
+# hash_matrix = confusion_matrix(y_test, y_pred_hash)
 
-# # print(cnf_matrix)
+# print(cnf_matrix)
 # print(tf_matrix)
 # # print(hash_matrix)
 
